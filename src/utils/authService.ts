16374c2d32b1b53c916e529dd427c9554c49e412
egjestad <egjestad@gmail.com>
2025-03-17 14:26:13 +0100

    interface and authservice classes

---
+++ src/utils/authService.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import type { User } from '@/models/interfaces'
+
+export async function getJwtToken(username: string, password: string): Promise<string | null> {
+  try {
+    const response = await axios.post('http://localhost:8080/login', {
+      username: username,
+      password: password,
+    })
+
+    if (response.status === 200) {
+      return response.data.token
+    }
+  } catch (error) {
+    console.error(error)
+  }
+  return null
+}
+
+export async function userDetails(username: string, token: string): Promise<User | null> {
+  try {
+    const response = await axios.get(`http://localhost:8080/user/${username}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+  return null
+}
