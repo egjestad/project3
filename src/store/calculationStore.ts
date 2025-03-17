17c882d07b5f2ce525e4bf2ca04b01d3016acc20
egjestad <egjestad@gmail.com>
2025-03-17 16:55:17 +0100

    Calculationstore

--- src/store/calculationStore.ts
+++ src/store/calculationStore.ts
@@ -1,24 +1,71 @@
 import { defineStore } from 'pinia'
+import type { Calculation } from '@/models/interfaces'
+import { ref } from 'vue'
+import { apiClient } from '@/utils/authService'
 
-export const useCalculationStore = defineStore('calculation', {
-  state: () => ({
-    userId: '',
-    calculation: '',
-    result: '',
-  }),
-  actions: {
-    updateCalculation(calculation: string) {
-      this.calculation = calculation
-    },
-    updateResult(result: string) {
-      this.result = result
-    },
-    updateUserId(userId: string) {
-      this.userId = userId
-    },
-    resetCalculation() {
-      this.calculation = ''
-      this.result = ''
-    },
-  },
+export const useCalculationStore = defineStore('calculation', () => {
+  const calculations = ref<Calculation[]>([])
+
+  function addCalculation(calculation: Calculation) {
+    if (calculations.value.length >= 10) {
+      calculations.value.shift()
+    }
+    calculations.value.push(calculation)
+  }
+
+  function getResult() {
+    if (calculations.value.length === 0) {
+      return 0
+    }
+    return calculations.value[calculations.value.length - 1].result
+  }
+
+  function getCalculations() {
+    return calculations.value
+  }
+
+  function setCalculations(newCalculations: Calculation[]) {
+    calculations.value = newCalculations
+  }
+
+  function clearCalculations() {
+    calculations.value = []
+  }
+
+  function saveToSessionStorage() {
+    sessionStorage.setItem('calculations', JSON.stringify(calculations.value))
+  }
+
+  function loadFromSessionStorage() {
+    const calculationsData = sessionStorage.getItem('calculations')
+    if (calculationsData) {
+      calculations.value = JSON.parse(calculationsData)
+    }
+  }
+
+  async function fetchCalculationsFromBackend() {
+    clearCalculations()
+    try {
+      const response = await apiClient.get('/recent')
+      if (response.status === 200) {
+        response.data.forEach((calculation: Calculation) => {
+          addCalculation(calculation)
+        })
+      }
+      saveToSessionStorage()
+    } catch (error) {
+      console.error('Error fetching recent calculations:', error)
+    }
+  }
+  return {
+    calculations,
+    addCalculation,
+    getResult,
+    getCalculations,
+    setCalculations,
+    clearCalculations,
+    saveToSessionStorage,
+    loadFromSessionStorage,
+    fetchCalculationsFromBackend,
+  }
 })
